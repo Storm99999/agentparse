39a09b9a67258b9fa9245fdd8f15a4773d2af9a6
gone <87811650+Storm99999@users.noreply.github.com>
2023-11-22 18:18:03 +0100

    Update agentparse.js

--- agentparse/src/agentparse.js
+++ agentparse/src/agentparse.js
@@ -1,15 +1,6 @@
-//////////////////////////////// AGENTPARSE  ///////////////////////////////////
-///                                                                        ///
-///   AgentParse, written by newguy - 2023, 22th Nov.                     ///
-///                                                                      ///
-///   Free to use in any of your future projects!                       ///
-/////////////////////////////////////////////////////////////////////////
-
-
-
-
 const parseAgent = (useragent) => {
-  const agent = (null, useragent).toString();
+  const agent = useragent.toLowerCase();
+
   // Operating System
   const os = agent.includes("windows") ? "Windows" : 
              agent.includes("mac os") ? "MacOS" :
@@ -17,23 +8,23 @@ const parseAgent = (useragent) => {
              agent.includes("android") ? "Android" :
              agent.includes("ios") ? "iOS" : "Unknown";
 
-    // Browser
-    const browser = agent.includes("chrome") ? "Google Chrome" :
-                    agent.includes("firefox") ? "Mozilla Firefox" :
-                    agent.includes("safari") ? "Apple Safari" :
-                    agent.includes("edge") ? "Microsoft Edge" :
-                    agent.includes("opera") ? "Opera" : "Unknown";
+  // Browser
+  const browser = agent.includes("chrome") ? "Google Chrome" :
+                  agent.includes("firefox") ? "Mozilla Firefox" :
+                  agent.includes("safari") ? "Apple Safari" :
+                  agent.includes("edge") ? "Microsoft Edge" :
+                  agent.includes("opera") ? "Opera" : "Unknown";
 
-    // Rendering Engine
-    const engine = agent.includes("webkit") ? "WebKit" :
-                   agent.includes("gecko") ? "Gecko" :
-                   agent.includes("trident") ? "Trident" :
-                   agent.includes("blink") ? "Blink" : "Unknown";
+  // Rendering Engine
+  const engine = agent.includes("webkit") ? "WebKit" :
+                 agent.includes("gecko") ? "Gecko" :
+                 agent.includes("trident") ? "Trident" :
+                 agent.includes("blink") ? "Blink" : "Unknown";
 
-  return { os, browser, engine }
+  return { os, browser, engine };
 };
 
-const userInfo = parseAgent(navigator.userAgent)
+const userInfo = parseAgent(navigator.userAgent || "Unknown UserAgent");
 const userInfoJSON = JSON.stringify(userInfo, null, 2);
 
 console.log(userInfoJSON);
